89c66d8ffd4f91abd403d8d072f3e45365cafddf
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-25 22:45:29 +0000

    prompt

--- .ccls-cache/@home@runner@HW9P5/main.cpp
+++ .ccls-cache/@home@runner@HW9P5/main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+ int matrix[2][2], i, j;
+  float Detr = 0;
+  cout << "Enter the top row of the matrix, the the bottem row of matrix: \n";
+  for (i = 0; i < 2: i++)
+  for (j = 0; j < 2; j++)
+    
+  
 }
--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
- 
+ int matrix[2][2], i, j;
+  float Detr = 0;
+  cout << "Enter the top row of the matrix, the the bottem row of matrix: \n";
+  for (i = 0; i < 2: i++)
+  for (j = 0; j < 2; j++)
+    
+  
 }
