93256cacd4dd52a1b7385842884d5e8cb9b22782
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-25 22:59:00 +0000

    finding determininate

--- main.cpp
+++ main.cpp
@@ -5,8 +5,19 @@ int main() {
  int matrix[2][2], i, j;
   float Detr = 0;
   cout << "Enter the top row of the matrix, the the bottem row of matrix: \n";
-  for (i = 0; i < 2: i++)
+  for (i = 0; i < 2; i++)
   for (j = 0; j < 2; j++)
-    
+  cin >> matrix[i][j];
+printf("\n Given martix is:");
+  for(i = 0; i < 2; i++)
+  {
+  cout<<"\n";
+  for (j = 0; j < 2; j++)
+  cout<< matrix[i][j]<<"\t";
+  }
+// finding the determinant
+  for ( i=0; i<2; i++)
+  Detr = Detr + (matrix[0][i] * ) 
+  
   
 }
