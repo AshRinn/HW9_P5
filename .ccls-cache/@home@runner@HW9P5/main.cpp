82b7e99b061cbaa8c94820126e429938343949b2
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-28 18:42:10 +0000

    found determinant of a

--- .ccls-cache/@home@runner@HW9P5/main.cpp
+++ .ccls-cache/@home@runner@HW9P5/main.cpp
@@ -4,20 +4,19 @@ using namespace std;
 int main() {
  int matrix[2][2], i, j;
   float Detr = 0;
-  cout << "Enter the top row of the matrix, the the bottem row of matrix: \n";
+  cout << "Enter the top row of the matrix, then the bottom row of matrix: \n";
   for (i = 0; i < 2; i++)
   for (j = 0; j < 2; j++)
   cin >> matrix[i][j];
-printf("\n Given martix is:");
+printf("\nGiven martix is:");
   for(i = 0; i < 2; i++)
   {
   cout<<"\n";
   for (j = 0; j < 2; j++)
   cout<< matrix[i][j]<<"\t";
   }
-// finding the determinant
-  for ( i=0; i<2; i++)
-  Detr = Detr + (matrix[0][i] * ) 
-  
-  
+// finding the determinant of a
+  Detr = 1/( (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
+   
+
 }
