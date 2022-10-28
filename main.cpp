0a3c1aadebda8e649694c735b61145d5710d5ff1
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-28 18:55:26 +0000

    found invers matix and out put it

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
  int matrix[2][2], i, j;
   float Detr = 0;
   int Adj[2][2];
+  int A_Inverse[2][2];
   cout << "Enter the top row of the matrix, then the bottom row of matrix: \n";
   for (i = 0; i < 2; i++)
   for (j = 0; j < 2; j++)
@@ -25,9 +26,17 @@ printf("\nGiven martix is:");
   Adj[1][1]= matrix[0][0];
   Adj[1][0]= -matrix[1][0];
 
-  
-  
-    
-   
+ // finding the invers matrix
+  A_Inverse[0][0]= Detr * Adj[0][0];
+  A_Inverse[0][1]= Detr * Adj[0][1];
+  A_Inverse[1][0]= Detr * Adj[1][0];
+  A_Inverse[1][1]= Detr * Adj[1][1];
+
+  for(i = 0; i < 2; i++)
+  {
+  cout<<"\n";
+  for (j = 0; j < 2; j++)
+  cout<< A_Inverse[i][j]<<"\t";
+  }
 
 }
